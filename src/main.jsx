bb10b3862012636c9580af528c41f0951b114aff
LearnerSumit <sumit.202104177@tulas.edu.in>
2025-07-23 15:54:48 +0530

    chanfe env and main

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import { ToastContainer } from 'react-toastify';
 
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <StrictMode>
     <ToastContainer />
     <App />
-  </>
+  </StrictMode>
 )
